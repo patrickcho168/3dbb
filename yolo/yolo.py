e4cebeb1dc55edccf88034cc7b34d09001b240fb
Patrick Cho <patcho@stanford.edu>
2019-06-03 23:59:02 -0700

    use custom trained yolo rather than pretrained yolo from coco

--- yolo/yolo.py
+++ yolo/yolo.py
@@ -8,6 +8,12 @@ source: https://www.pyimagesearch.com/2018/11/12/yolo-object-detection-with-open
 import cv2
 import numpy as np
 import os
+from models import *
+import torch
+import torchvision.transforms as transforms
+from PIL import Image
+from utils.datasets import pad_to_square, resize
+from utils.utils import *
 
 class cv_Yolo:
 
@@ -15,75 +21,100 @@ class cv_Yolo:
         self.confidence = confidence
         self.threshold = threshold
 
-        labels_path = os.path.sep.join([yolo_path, "coco.names"])
+        # labels_path = os.path.sep.join([yolo_path, "coco.names"])
+        labels_path = os.path.sep.join([yolo_path, "classes.names"])
         self.labels = open(labels_path).read().split("\n")
 
         np.random.seed(42)
         self.colors = np.random.randint(0,255, size=(len(self.labels), 3), dtype="uint8")
 
-        weights_path = os.path.sep.join([yolo_path, "yolov3.weights"])
-        cfg_path = os.path.sep.join([yolo_path, "yolov3.cfg"])
+        #weights_path = os.path.sep.join([yolo_path, "yolov3.weights"])
+        #cfg_path = os.path.sep.join([yolo_path, "yolov3.cfg"])
+        weights_path = os.path.sep.join([yolo_path, "exp1_ckpt_8.pth"])
+        cfg_path = os.path.sep.join([yolo_path, "yolov3-custom.cfg"])
+        self.net = Darknet(cfg_path).to('cuda') # OR cpu
+        self.net.load_state_dict(torch.load(weights_path))
+        #self.net = cv2.dnn.readNetFromDarknet(cfg_path, weights_path)
 
-        self.net = cv2.dnn.readNetFromDarknet(cfg_path, weights_path)
-
-    def detect(self, image):
+    def detect(self, img_path):
         # assert image is opencv
-        (H,W) = image.shape[:2]
+        # (H,W) = image.shape[:2]
 
-        ln = self.net.getLayerNames()
-        ln = [ln[i[0] - 1] for i in self.net.getUnconnectedOutLayers()]
+        #ln = self.net.getLayerNames()
+        #ln = [ln[i[0] - 1] for i in self.net.getUnconnectedOutLayers()]
 
         # prepare input
-        blob = cv2.dnn.blobFromImage(image, 1 / 255.0, (416, 416), swapRB=True, crop=False)
+        # blob = cv2.dnn.blobFromImage(image, 1 / 255.0, (416, 416), swapRB=True, crop=False)
+        # blob = torch.Tensor(blob).to('cuda')
 
-        self.net.setInput(blob)
-        output = self.net.forward(ln)
+        img = transforms.ToTensor()(Image.open(img_path).convert('RGB'))
 
+        _, H, W = img.shape
+        h_factor, w_factor = (H, W)
+        # Pad to square resolution
+        img, pad = pad_to_square(img, 0)
+        _, padded_h, padded_w = img.shape
+        imgs = torch.stack([resize(img_, 416) for img_ in [img]]).to('cuda')
+
+        #self.net.setInput(blob)
+        #output = self.net.forward(ln)
+        with torch.no_grad():
+            output = self.net(imgs)
+            output = non_max_suppression(output, 0.5, 0.5)[0]
+        # output = self.net.forward(imgs).numpy()
+        output = rescale_boxes(output, 416, (H, W)).numpy()
         detections = []
 
         boxes = []
         confidences = []
         class_ids = []
 
-        for output in output:
-            for detection in output:
-                scores = detection[5:]
-                class_id = np.argmax(scores)
-                confidence = scores[class_id]
+        # for output in output:
+        #for detection in output:
+        for x1, y1, x2, y2, conf, cls_conf, cls_pred in output:
+                #scores = detection[5:]
+                #class_id = np.argmax(scores)
+                #confidence = 1.0 # min(detection[4], scores[class_id])
 
-                if confidence > self.confidence:
+                #if confidence > self.confidence:
 
-                    box = detection[0:4] * np.array([W, H, W, H])
-                    (centerX, centerY, width, height) = box.astype("int")
-
+                    #box = detection[0:4] / np.array([416, 416, 416, 416]) * np.array([W, H, W, H])
+                    #(centerX, centerY, width, height) = box.astype("int")
                     # use the center (x, y)-coordinates to derive the top and
                     # and left corner of the bounding box
-                    x = int(centerX - (width / 2))
-                    y = int(centerY - (height / 2))
+                    width = x2 - x1
+                    height = y2 - y1
+                    x = x1#int(centerX - (width / 2))
+                    y = y1#int(centerY - (height / 2))
 
                     # update our list of bounding box coordinates, confidences,
                     # and class IDs
 
                     boxes.append([x, y, int(width), int(height)])
-                    confidences.append(float(confidence))
-                    class_ids.append(class_id)
+                    confidences.append(float(cls_conf))
+                    class_ids.append(int(cls_pred))
+                    class_ = self.get_class(int(cls_pred))
+                    top_left = (int(x1), int(y1))
+                    bottom_right = (int(x2), int(y2))
+                    box_2d = [top_left, bottom_right]
+                    detections.append(Detection(box_2d, class_))
 
 
 
-        idxs = cv2.dnn.NMSBoxes(boxes, confidences, self.confidence, self.threshold)
+        #idxs = cv2.dnn.NMSBoxes(boxes, confidences, self.confidence, self.threshold)
 
-        if len(idxs) > 0:
-            for i in idxs.flatten():
+        #if len(idxs) > 0:
+        #    for i in idxs.flatten():
 
-                top_left = (boxes[i][0], boxes[i][1])
-                bottom_right = (top_left[0] + boxes[i][2], top_left[1] + boxes[i][3])
+        #        top_left = (boxes[i][0], boxes[i][1])
+        #        bottom_right = (top_left[0] + boxes[i][2], top_left[1] + boxes[i][3])
 
-                box_2d = [top_left, bottom_right]
-                class_ = self.get_class(class_ids[i])
-                if class_ == "person":
-                    class_ = "pedestrian"
+        #        box_2d = [top_left, bottom_right]
+        #        class_ = self.get_class(class_ids[i])
+        #        if class_ == "person":
+        #            class_ = "pedestrian"
 
-                detections.append(Detection(box_2d, class_))
+        #        detections.append(Detection(box_2d, class_))
 
         return detections
 
